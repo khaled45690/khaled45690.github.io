dbecb7c851b7c946fd01b908c720a0e94db42b98
khaled45690 <k.saad1281@gmail.com>
2024-08-10 11:13:09 +0300

    first commit

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { color, motion } from "framer-motion";
+
+import { services } from "../../constants";
+import { SectionWrapper } from "../../hoc";
+import { fadeIn } from "../../utils/motion";
+import { config } from "../../constants/config";
+import { Header } from "../atoms/Header";
+
+interface IServiceCard {
+  index: number;
+  title: string;
+  icon: string;
+}
+
+const ServiceCard: React.FC<IServiceCard> = ({ index, title, icon }) => (
+  <Tilt
+    glareEnable
+    tiltEnable
+    tiltMaxAngleX={30}
+    tiltMaxAngleY={30}
+    glareColor="#aaa6c3"
+  >
+    <div className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className="green-pink-gradient shadow-card w-full rounded-[20px] p-[1px]"
+      >
+        <div className="bg-tertiary flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] px-12 py-5">
+          <img
+            src={icon}
+            alt="web-development"
+            className="h-16 w-16 object-contain"
+          />
+
+          <h3 className="text-center text-[20px] font-bold text-white">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <Header useMotion={true} {...config.sections.about} />
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="text-secondary mt-4 max-w-3xl text-[17px] leading-[30px]"
+      >
+        A highly motivated and solution-oriented Flutter Developer with over 6 years of experience 
+      in building innovative mobile and desktop applications. Known for a keen interest in learning new skills and a proactive 
+      approach to problem-solving. Demonstrated ability to plan and execute projects efficiently. Recently I participated in contributions 
+      for the flutter community by creating C++ plugin check it out on <a href="https://pub.dev/packages/yuv_to_png"> pub.dev</a>, and participated in expanding my skill set to include 
+      Go projects. Committed to delivering high-quality results and eager to bring my unique blend of skills and experience to a dynamic and 
+      innovative team,
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10 max-sm:justify-center">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
